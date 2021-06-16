d49ac4c90a47f048ab44f6a30fc2b55249193dd1
ICascha <caschavanwanrooij@gmail.com>
2021-06-16 13:38:07 +0200

    Update metrics.py

--- backend/metrics.py
+++ backend/metrics.py
@@ -21,4 +21,4 @@ def cross_acf(x, y, k, le=False):
         arr **=2
     b = (arr - np.nanmean(arr, axis=1, keepdims=True))
 
-    return np.nansum((a * b), axis=1) / np.sqrt(np.nansum(a**2, axis=0) * np.nansum(b**2, axis=1))unb
+    return np.nansum((a * b), axis=1) / np.sqrt(np.nansum(a**2, axis=0) * np.nansum(b**2, axis=1))
