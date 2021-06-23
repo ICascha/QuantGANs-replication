097b28d501e64f27c72c7df0a17d90c90251a7e1
ICascha <caschavanwanrooij@gmail.com>
2021-06-23 08:54:04 +0200

    Add files via upload

--- backend/gan.py
+++ backend/gan.py
@@ -2,10 +2,11 @@ from tensorflow.keras.losses import BinaryCrossentropy
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.utils import Progbar
 from tensorflow.keras.models import load_model
+from tensorflow import convert_to_tensor
+from math import floor, ceil
 
 import pickle
 import tensorflow as tf
-import tensorflow.keras.backend as K
 import numpy as np
 
 class GAN:
@@ -100,3 +101,69 @@ class GAN:
            Acces to the instance namespace is provided.
         """        
         pass
+
+class CGAN(GAN):
+    """ conditonal generative adverserial network class for conditional tcns.
+    Training code for a standard DCGAN using the Adam optimizer.
+    """    
+
+    def __init__(self, discriminator, generator, training_input, lr_d=1e-4, lr_g=3e-4, epsilon=1e-8, beta_1=.0, beta_2=0.9, from_logits=True):
+        """Create a GAN instance
+        Args:
+            discriminator (tensorflow.keras.models.Model): Discriminator model.
+            generator (tensorflow.keras.models.Model): Generator model.
+            training_input (int): input size of temporal axis of noise samples.
+            lr_d (float, optional): Learning rate of discriminator. Defaults to 1e-4.
+            lr_g (float, optional): Learning rate of generator. Defaults to 3e-4.
+            epsilon (float, optional): Epsilon paramater of Adam. Defaults to 1e-8.
+            beta_1 (float, optional): Beta1 parameter of Adam. Defaults to 0.
+            beta_2 (float, optional): Beta2 parameter of Adam. Defaults to 0.9.
+            from_logits (bool, optional): Output range of discriminator, logits imply output on the entire reals. Defaults to True.
+        """
+        super().__init__(discriminator, generator, training_input, lr_d=1e-4, lr_g=3e-4, epsilon=1e-8, beta_1=.0, beta_2=0.9, from_logits=True)
+
+        # build a generator model that left-appends conditional information
+        input_layer = Input(shape=self.noise_shape)
+        concat_to_output = Input(shape=[self.noise_shape[0]//2, floor(self.noise_shape[1]/2), 1])
+        output_generator = self.generator(input_layer)
+        output_layer = Concatenate(axis=2)([output_generator, concat_to_output])
+        self.cond_generator = Model([input_layer, concat_to_output], output_layer)
+
+    @tf.function
+    def train_step(self, data, batch_size, additional_d_steps):
+
+        for _ in range(additional_d_steps + 1):
+            noise = tf.concat([tf.zeros([batch_size, self.noise_shape[0]//2, floor(self.noise_shape[1]/2), 3]),
+                tf.random.normal([batch_size, self.noise_shape[0]//2, ceil(self.noise_shape[1]/2), 3])], axis=2)
+
+            real_part = tf.repeat(tf.concat([convert_to_tensor(data[:, :, :floor(self.noise_shape[1]/2), :]), 
+                tf.zeros([batch_size, self.noise_shape[0]//2, ceil(self.noise_shape[1]/2), 1])], axis=2), 3, axis=3)
+
+            noise_input = tf.concat([real_part, noise], axis=1)
+
+            generated_data = self.cond_generator([noise_input, data[:, :, :floor(self.noise_shape[1]/2), :]],
+                training=False)
+
+            with tf.GradientTape() as disc_tape:
+                real_output = self.discriminator(data, training=True)
+                fake_output = self.discriminator(generated_data, training=True)
+                disc_loss = self.discriminator_loss(real_output, fake_output)
+            
+            gradients_of_discriminator = disc_tape.gradient(disc_loss, self.discriminator.trainable_variables)
+            self.discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, self.discriminator.trainable_variables))
+
+        noise = tf.concat([tf.zeros([batch_size, self.noise_shape[0]//2, floor(self.noise_shape[1]/2), 3]),
+            tf.random.normal([batch_size, self.noise_shape[0]//2, ceil(self.noise_shape[1]/2), 3])], axis=2)
+
+        real_part = tf.repeat(tf.concat([convert_to_tensor(data[:, :, :floor(self.noise_shape[1]/2), :]), 
+            tf.zeros([batch_size, self.noise_shape[0]//2, ceil(self.noise_shape[1]/2), 1])], axis=2), 3, axis=3)
+
+        noise_input = tf.concat([real_part, noise], axis=1)
+
+        with tf.GradientTape() as gen_tape:
+            generated_data = self.cond_generator([noise_input, data[:, :, :floor(self.noise_shape[1]/2), :]],
+            training=True)
+            fake_output = self.discriminator(generated_data, training=False)
+            gen_loss = self.generator_loss(fake_output)
+            gradients_of_generator = gen_tape.gradient(gen_loss, self.cond_generator.trainable_variables)
+            self.generator_optimizer.apply_gradients(zip(gradients_of_generator, self.cond_generator.trainable_variables))
